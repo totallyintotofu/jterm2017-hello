eb2e4546a78e398560b4471f7f7ffd19aab6ed8f
totallyintotofu <chen33l@mtholyoke.edu>
2017-01-04 13:11:41 -0500

    Country Lookup App
    Looks up three countries.

--- Jterm2017HelloWorld/app/src/main/java/com/example/demouser/jterm2017helloworld/MainActivity.java
+++ Jterm2017HelloWorld/app/src/main/java/com/example/demouser/jterm2017helloworld/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.demouser.jterm2017helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Map<String, String> countryCodes = new HashMap<>();
+
+    static
+    {
+        countryCodes.put("GBR", "United Kingdom");
+        countryCodes.put("IDN", "Indonesia");
+        countryCodes.put("IND", "India");
+
+    }
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((Button)findViewById(R.id.lookupButton)).setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                lookup();
+            }
+        });
+
+    }
+
+    public void lookup(){
+        String code = ((EditText)findViewById(R.id.editText)).getText().toString().toUpperCase();
+        String country;
+        if(countryCodes.containsKey(code))
+        {
+            country = countryCodes.get(code);
+
+        }
+        else
+        {
+            country = code;
+        }
+
+        ((TextView)findViewById(R.id.countryCode)).setText(country);
     }
 }
